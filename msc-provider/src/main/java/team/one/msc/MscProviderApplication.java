edef39f32f3784eb1f3e8c2ceaa30201114a86d9
lankk1 <835264673@qq.com>
2020-05-12 18:17:18 +0800

    provider初始创建

---
+++ msc-provider/src/main/java/team/one/msc/MscProviderApplication.java
@@ -0,0 +1,13 @@
+package team.one.msc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscProviderApplication.class, args);
+	}
+
+}
---
+++ msc-provider/src/test/java/team/one/msc/MscProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package team.one.msc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
