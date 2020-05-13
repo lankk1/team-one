c4315dfe1ed3b79007239e340d8988be2878e018
lankk1 <835264673@qq.com>
2020-05-13 18:24:33 +0800

    provider搭建

--- msc-provider/src/main/java/team/one/msc/MscProviderApplication.java
+++ msc-provider/src/main/java/team/one/msc/MscProviderApplication.java
@@ -2,12 +2,14 @@ package team.one.msc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MscProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MscProviderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MscProviderApplication.class, args);
+    }
 
 }
---
+++ msc-provider/src/main/java/team/one/msc/provider/controller/ProviderController.java
@@ -0,0 +1,32 @@
+package team.one.msc.provider.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lankk on 2020/5/13.
+ *
+ * @author lankk
+ * @date 2020/5/13
+ */
+@Slf4j
+@RestController
+@RequestMapping("/pvd")
+public class ProviderController {
+    /**
+     * ${provider.unique.flag:NULL-flag} 表示 只有provider.unique.flag为null时，providerFlag 的值为 NULL-flag
+     * ，否则 providerFlag 为 provider.unique.flag 的值
+     */
+    @Value("${provider.unique.flag:NULL-flag}")
+    private String providerFlag;
+
+    @GetMapping("/test/{id}")
+    public String testId(@PathVariable int id) {
+        log.info("ProviderController.testId,id={}", id);
+        return providerFlag + ",provide id=" + id;
+    }
+}
